7e918455a91e8394c35108fc5ce1519e31f3fcaf
mk-munna <mkmunnaofficial@gmail.com>
2024-03-15 21:56:09 +0600

    setup done first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button class="btn btn-secondary">Secondary</button>
+    </>
+  )
+}
+
+export default App
